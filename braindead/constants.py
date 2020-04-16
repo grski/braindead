e56729a1c03a04a12ccaf1b076a3dcabcd5bc7a1
Olaf Górski <olafgorski@protonmail.com>
2020-04-16 22:33:24 +0200

    [feature/new-config] read config from pyproject.toml instead of custom, new file to comply with pep standards

--- braindead/constants.py
+++ braindead/constants.py
@@ -2,16 +2,20 @@ import os
 
 import toml
 
+USER_CONFIG_FILE = "pyproject.toml"
+
+
+def get_user_config():
+    if not os.path.exists(USER_CONFIG_FILE):
+        return {}
+    return toml.load(USER_CONFIG_FILE).get("tool", {}).get("braindead", {})
+
+
 PACKAGE_LOCATION: str = os.path.split(os.path.realpath(__file__))[0]
-
-# todo: add defualts/overwriting of site.toml
-DEFAULT_CONFIG: dict = toml.load(os.path.join(PACKAGE_LOCATION, "site.toml"))
-# todo: add validation of the toml?
-NEW_CONFIG: dict = {} if not os.path.exists("site.toml") else toml.load("site.toml")
-CONFIG: dict = {**DEFAULT_CONFIG, **NEW_CONFIG}
-BASE_URL: str = CONFIG["BASE_URL"]
-DIST_DIR: str = CONFIG.get("DIST_DIR", "dist")
-TEMPLATE_DIR: str = CONFIG.get("TEMPLATE_DIR")
-
-# todo other template flow (git clone?)
-TEMPLATE_DIR = TEMPLATE_DIR if TEMPLATE_DIR else os.path.join(PACKAGE_LOCATION, "default_template")
+DEFAULT_CONFIG: dict = toml.load(os.path.join(PACKAGE_LOCATION, "default_config.toml"))
+USER_CONFIG: dict = get_user_config()
+CONFIG: dict = {**DEFAULT_CONFIG, **USER_CONFIG}
+BASE_URL: str = CONFIG["site"]["base_url"]
+DIST_DIR: str = CONFIG["site"].get("dist_dir", "dist")
+# todo: add git cloning of templates
+TEMPLATE_DIR: str = CONFIG.get("template_dir", os.path.join(PACKAGE_LOCATION, "default_template"))
